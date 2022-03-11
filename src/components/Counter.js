5168159478fc547c74d2ba63353564387157d2a4
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-03-11 14:49:42 +0530

    wired up the redux provider with the app

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
+
+  const dispatch =  useDispatch();
+  const counter = useSelector(state => state.counter);
+
+  const incHandler = () =>{
+    dispatch({
+      type: 'INC',
+    })
+  }
+  const decHandler = () =>{
+    dispatch({
+      type: 'DEC',
+    })
+  }
+  
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+
+        <button onClick={decHandler}>-</button>
+        <button onClick={incHandler}>+</button>
+        
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
